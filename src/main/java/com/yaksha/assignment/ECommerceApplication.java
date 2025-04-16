8b678dda0ed253910b6d4e9cb0fccadfd367e5c7
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-16 09:06:11 +0530

    Update ECommerceApplication.java

--- src/main/java/com/yaksha/assignment/ECommerceApplication.java
+++ src/main/java/com/yaksha/assignment/ECommerceApplication.java
@@ -8,18 +8,6 @@ import org.springframework.context.ApplicationContext;
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
-		// Run the Spring Boot application
-		ApplicationContext context = SpringApplication.run(ECommerceApplication.class, args);
-
-		// Retrieving beans from the Spring container
-		Customer customer = context.getBean(Customer.class);
-		Order order = context.getBean(Order.class);
-
-		// Display customer and order details
-		System.out.println("Customer: " + customer.getName() + " (" + customer.getCustomerId() + ")");
-		System.out.println("Order ID: " + order.getOrderId() + ", Total Amount: " + order.getTotalAmount());
-
-		// Place an order
-		customer.placeOrder();
+		// write your logic here
 	}
 }

76d819257a397a624f74607177ba7b33fce9a8d9
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-18 21:43:43 +0530

    Update Customer.java

--- src/main/java/com/yaksha/assignment/Customer.java
+++ src/main/java/com/yaksha/assignment/Customer.java
@@ -1,5 +1,43 @@
 package com.yaksha.assignment;
 
 public class Customer {
-	// write your logic here
+
+	private String name;
+	private String customerId;
+
+	private Order order;
+
+	public Customer() {
+		this.name = "John Doe";
+		this.customerId = "CUST001";
+	}
+
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public void placeOrder() {
+		System.out.println(name + " placed an order: " + order.getOrderId());
+	}
 }

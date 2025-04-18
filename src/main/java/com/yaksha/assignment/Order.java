ad517a050125967b6823bef741262317edb2f8eb
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-18 21:44:09 +0530

    Update Order.java

--- src/main/java/com/yaksha/assignment/Order.java
+++ src/main/java/com/yaksha/assignment/Order.java
@@ -1,5 +1,39 @@
 package com.yaksha.assignment;
 
 public class Order {
-	// write your logic here
+
+	private String orderId;
+	private double totalAmount;
+
+	private Product product;
+
+	public Order() {
+		this.orderId = "ORD12345";
+		this.totalAmount = 1000.0;
+	}
+
+	// Getters and Setters
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
 }
